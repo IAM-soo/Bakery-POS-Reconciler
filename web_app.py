a72ec36a239763179125530d6750908ea6b68b92
IAM-soo <phkevin.soo@gmail.com>
2026-05-20 11:25:05 +0900

    Add emoney calcalate on web

--- web_app.py
+++ web_app.py
@@ -12,3 +12,13 @@ st.title("Bakery POS Reconciler")
 
 products = load_menu()
 st.write("商品数:", len(products))
+
+st.header("差額チェック")
+pos_amount = st.number_input("POS金額", min_value=0, step=1)
+
+cat_emoney = {"楽天Edy": 0, "ID":0, "QUICPay":0, "WAON":0, "nanaco":0}
+for emoney_name in cat_emoney:
+    cat_emoney[emoney_name] = st.number_input(emoney_name, min_value=0, step=1)
+
+emoney_total = calculate_payment_total(cat_emoney)
+st.write("CAT電子マネー総額:", emoney_total)
